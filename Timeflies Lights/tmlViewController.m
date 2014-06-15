ec8b18222269b68b8b3492b60ee97d36dbbd6a52
Rayce Stipanovich <stip.rayce@gmail.com>
2014-06-15 15:54:34 -0400

    base code

--- Timeflies Lights/tmlViewController.m
+++ Timeflies Lights/tmlViewController.m
@@ -7,17 +7,56 @@
 //
 
 #import "tmlViewController.h"
+#import "GCDAsyncUdpSocket.h"
 
 @interface tmlViewController ()
-
+{
+	long tag;
+	GCDAsyncUdpSocket *udpSocket;
+}
 @end
 
 @implementation tmlViewController
 
+- (void)setupSocket
+{
+	udpSocket = [[GCDAsyncUdpSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
+
+	NSError *error = nil;
+
+	if (![udpSocket bindToPort:7135 error:&error])
+	{
+		return;
+	}
+	if (![udpSocket beginReceiving:&error])
+	{
+		return;
+	}
+
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+	if (udpSocket == nil)
+	{
+		[self setupSocket];
+	}
+
+
+}
+
+- (void)udpSocket:(GCDAsyncUdpSocket *)sock didReceiveData:(NSData *)data
+	  fromAddress:(NSData *)address
+withFilterContext:(id)filterContext
+{
+	NSString *msg = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+	if (msg)
+	{
+		//parse our UDP data here
+		
+	}
 }
 
 - (void)didReceiveMemoryWarning
@@ -27,3 +66,5 @@
 }
 
 @end
+
+
